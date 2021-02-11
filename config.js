abbc707ec77a3c12af0bb63d36bb2ddd2e3d4b98
Giulia Ferrari <giulia.ferrari0917@gmail.com>
2021-02-11 18:19:41 +0100

    add the base

---
+++ config.js
@@ -0,0 +1,92 @@
+var config = {
+    style: 'mapbox://styles/giuls0917/ckjyjuv7y1ux717s4qbovpev9',
+    accessToken: 'pk.eyJ1IjoiZ2l1bHMwOTE3IiwiYSI6ImNranlqM2RpYzBzMTcybm53NDcyMHhleWwifQ.qqOs_Y1d36x-WQYI_sww5g',
+    showMarkers: false,
+    markerColor: '#3FB1CE',
+    theme: 'dark',
+    use3dTerrain: true,
+    title: 'Cities growth and the importance of biodiversity',
+    subtitle: 'Barcelona and xx case',
+    byline: 'By Giulia Ferrari',
+    footer: 'Source: source citations, etc.',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            alignment: 'left',
+            hidden: true,
+            title: 'Did you know?',
+            image: './path/to/image/source.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            location: {
+                center: [6.296448, 35.881480],
+                zoom: 1.5,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                //layer: 'cities_source',
+                //opacity: 1,
+                //duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'slug-style-id',
+            alignment: 'left',
+            hidden: false,
+            title: 'Display Title',
+            image: './path/to/image/source.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            location: {
+                center: [-122.418398, 37.759483],
+                zoom: 8.5,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'other-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: 'Second Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-77.020636, 38.886900],
+                zoom: 8.5,
+                pitch: 60,
+                bearing: -43.2
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
